0cfac3e3cb0d0dcb6cbbd78e0ac5b4974aeeeb89
serkanXman <msozkilic@gmail.com>
2023-04-13 23:36:20 +0200

    check

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -2,6 +2,7 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
 
@@ -43,6 +44,21 @@ public class ZippoTest {
                 .contentType(ContentType.JSON)
 
         ;
+    }
+    @Test
+    public void checkStateInResponseBody(){
+
+                given()//todo hazirlik islemleri token ,send , body , parametreler
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then() // todo dogrulama yeri ,statüs
+                .log().body() //todo log.All bütün responsu sonucu listeyi verir
+                .body("country",equalTo("United States"))//todo body.country==United States ?
+                .statusCode(200)
+
+                ;
     }
 
 }
