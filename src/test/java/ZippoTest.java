0e22189d0df03761d45a5d904512d031bbacab4a
serkanXman <msozkilic@gmail.com>
2023-04-17 23:50:30 +0200

    status code

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -22,5 +22,20 @@ public class ZippoTest {
                 .then();// todo assertion ve verileri ele alma ,extract
     }
 
+    @Test
+    public void statusCodeTest(){
+
+        given()//todo hazirlik islemleri token ,send , body , parametreler
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then() // todo dogrulama yeri ,statüs
+                .log().body() //todo log.All bütün responsu sonucu listeyi verir
+                .statusCode(200)
+
+        ;
+    }
+
 
 }
