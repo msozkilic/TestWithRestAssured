94b2086c8bac2b4dc34f5ee896dda696c3f56b3a
serkanXman <msozkilic@gmail.com>
2023-04-25 11:19:46 +0200

    hasItem

--- src/test/java/ZippoÜben.java
+++ src/test/java/ZippoÜben.java
@@ -3,6 +3,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 
 public class ZippoÜben {
 
@@ -49,12 +50,25 @@ public class ZippoÜben {
 
                 .then()
                 .log().body()
-                .body("country",equalTo("United States" ))
+                .body("country",equalTo("United State"))
                 .statusCode(200)
                 .contentType(ContentType.JSON)
+        ;
+    }
+    @Test
+    public void bodyJsonPathTest(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/tr/01000")
 
 
-                ;
+                .then()
+                .log().body()
+                .body("places.'place name'",hasItem("Camuzcu Köyü"))
+                .statusCode(200)
+
+        ;
     }
 
 
