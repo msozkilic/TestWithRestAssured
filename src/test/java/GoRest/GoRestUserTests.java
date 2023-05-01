2a8e62f4ee5791777697c9d90b26d3429d62dd6e
serkanXman <msozkilic@gmail.com>
2023-05-01 10:51:58 +0200

    createObject

--- src/test/java/GoRest/GoRestUserTests.java
+++ src/test/java/GoRest/GoRestUserTests.java
@@ -74,11 +74,11 @@ public class GoRestUserTests {
     @Test
     public void createObject(){
 
-        Map<String,String> newUser=new HashMap<>();
-        newUser.put("name",getRandomName());
-        newUser.put("gender","male");
-        newUser.put("email",getRandomEmail());
-        newUser.put("status","active");
+        User newUser=new User();
+        newUser.setName(getRandomName());
+        newUser.setEmail(getRandomEmail());
+        newUser.setGender("male");
+        newUser.setStatus("active");
 
         int userID=
                 given()
@@ -98,6 +98,45 @@ public class GoRestUserTests {
                 ;
         System.out.println("userID ="+ userID);
 
+    }
+    class User{
+        private String name;
+        private String gender;
+
+        private  String email;
+        private String status;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
     }
 
 }
