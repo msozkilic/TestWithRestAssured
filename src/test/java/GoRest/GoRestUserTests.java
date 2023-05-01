bc1f458fbf6687c08fcf754326d3178e15d2cca5
serkanXman <msozkilic@gmail.com>
2023-05-01 10:39:03 +0200

    gorestApiTest_Map

--- src/test/java/GoRest/GoRestUserTests.java
+++ src/test/java/GoRest/GoRestUserTests.java
@@ -37,6 +37,27 @@ public class GoRestUserTests {
     return RandomStringUtils.randomAlphabetic(8);
     }
     public String getRandomEmail(){
-    return RandomStringUtils.randomAlphabetic(8)+"gmail.com";
+    return RandomStringUtils.randomAlphabetic(8)+"@gmail.com";
     }
+    @Test
+    public void createUserMap(){
+        int userID=
+                given()
+                        .header("Authorization","Bearer c2668e9cfb33f884ca5b66f5cc8e8acba4e2b151e47c88a362113bef8d6edbd9")
+                        .contentType(ContentType.JSON)
+                        .body("{\"name\":\""+getRandomName()+"\", \"gender\":\"Male\", \"email\":\"\"+getRandomEmail()+\"\", \"status\":\"Active\"}")
+
+                        .when()
+                        .post("users")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id")
+                ;
+        System.out.println("userID ="+ userID);
+
+    }
+
 }
