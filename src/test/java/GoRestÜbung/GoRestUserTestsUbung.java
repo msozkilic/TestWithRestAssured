6945bc83a64bbabbf6c581ddf6adb7a344406ce9
serkanXman <msozkilic@gmail.com>
2023-05-08 21:53:16 +0200

    updateUserr

--- src/test/java/GoRestÜbung/GoRestUserTestsUbung.java
+++ src/test/java/GoRestÜbung/GoRestUserTestsUbung.java
@@ -8,6 +8,8 @@ import org.testng.annotations.Test;
 
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class GoRestUserTestsUbung {
 
     @BeforeClass
@@ -15,6 +17,8 @@ public class GoRestUserTestsUbung {
 
         baseURI="https://gorest.co.in/public/v2/";
     }
+
+   //todo ****************************************************
     public String getRandomName(){
 
         return RandomStringUtils.randomAlphabetic(8).toLowerCase();
@@ -23,17 +27,22 @@ public class GoRestUserTestsUbung {
     public String getRandomEmail(){
         return RandomStringUtils.randomAlphabetic(8).toLowerCase()+"@gmail.com";
     }
+    //todo********************************************************
 
+
+
+    int userId=0;
+    Userr userr;
     @Test
     public void createUserObject() {
 
-        Userr userr = new Userr();
+
         userr.setName(getRandomName());
         userr.seteMail(getRandomEmail());
         userr.setGender("male");
         userr.setStatus("active");
 
-        int userrId=
+        userId=
 
         given()
                 .header("Authorization", "Bearer c2668e9cfb33f884ca5b66f5cc8e8acba4e2b151e47c88a362113bef8d6edbd9")
@@ -48,14 +57,36 @@ public class GoRestUserTestsUbung {
 
                 .then()
                 .log().body()
-                .contentType(ContentType.JSON)
                 .statusCode(201)
-                .extract().path("id")
+                .contentType(ContentType.JSON)
+                .extract().jsonPath().getInt("id")
 
 
                 ;
-        System.out.println("userrId = "+userrId);
+        System.out.println("userrId = "+userId);
 
+    }
+    @Test(dependsOnMethods = "createUserObject",priority = 1)
+    public void updateUserr(){
+
+       userr.setName("Serkan Baskan");
+        given()
+                .header("Authorization", "Bearer c2668e9cfb33f884ca5b66f5cc8e8acba4e2b151e47c88a362113bef8d6edbd9")
+                .body(userr)
+                .contentType(ContentType.JSON)
+                .param("UserId",userId)
+
+                .when()
+                .put("users/{UserId}")
+
+
+                .then()
+                .contentType(ContentType.JSON)
+                .log().body()
+                .statusCode(200)
+                .body("userId",equalTo("Serkan Baskan"))
+
+                ;
     }
 
 }
