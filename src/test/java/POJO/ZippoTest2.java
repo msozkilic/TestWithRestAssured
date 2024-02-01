3b53c033688b937f547c9fd61a3c5ba7adbf351b
serkanXman <msozkilic@gmail.com>
2024-02-01 16:24:15 +0100

    checkInResponseBody

--- src/test/java/POJO/ZippoTest2.java
+++ src/test/java/POJO/ZippoTest2.java
@@ -3,7 +3,9 @@ package POJO;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ZippoTest2 {
 
@@ -42,6 +44,24 @@ public class ZippoTest2 {
                .statusCode(200)
                .contentType(ContentType.JSON)
 
+
+               ;
+    }
+
+    @Test
+    public void checkStateInResponseBody(){
+
+       given()
+
+
+               .when()
+               .get("http://api.zippopotam.us/us/90210")
+
+               .then()
+               .contentType(ContentType.JSON)
+               .log().body()
+               .body("country",equalTo("unitedStates"))
+               .statusCode(200)
 
                ;
     }
